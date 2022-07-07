f852ca82b8d63591abf2196e441805b2ea5b0204
PTAHume <102745939+PTAHume@users.noreply.github.com>
2022-07-07 14:35:26 +0100

    fixed country

--- src/main-page/house-filter.tsx
+++ src/main-page/house-filter.tsx
@@ -1,11 +1,21 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 const HouseFilter = ({ allHouses }: { allHouses: any }) => {
   const history = useNavigate();
-  const {country} = useParams();
   const countries = allHouses
     ? Array.from(new Set(allHouses.map((h: any) => h.country)))
     : [];
   countries.unshift(null);
+  const location = useLocation();
+  const [country, setCountry] = useState("");
+  useEffect(() => {
+    if (location.pathname.lastIndexOf("/") > 0) {
+      setCountry(location.pathname.split("/")[2]);
+    }else{
+      setCountry("");
+    }
+    console.log(country);
+  }, [location, country]);
   const onSearchChange = (e: any) => {
     const countryVal = e.target.value;
     history(`/SearchResults/${countryVal}`);
@@ -13,7 +23,7 @@ const HouseFilter = ({ allHouses }: { allHouses: any }) => {
   return (
     <div className="row mt-3">
       <div className="offset-md-2 col-md-4">
-        Look for your dream house in country {country}:
+        Look for your dream house in country:
       </div>
       <div className="col-md-4 mb-3">
         <select
